9e8f74d7010ded5867075c557a9274611febbe8b
Mike Nezzer <mnezz1131@gmail.com>
2020-02-15 09:15:46 -0600

    working on get read function

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # liri-node-app
+
+The Liri-Node-App was designed to be multi use entertainment and information application to return search resuls on Movies from OMDB, Songs from Spotify and Concerts from Bands in Town.
+
+The code in liri.js is organized in distinct sections and grouped together according to what the app is going at any given moment.
+Lines 1 - 9 are grouping and setting varaibles for all the required packages and files.
+
+LInes 10-16 sets up variables to direct and capture the user's input.
+
+Lines 18-42 ia function that will run the program and take the user command and input and run it  through the function until it hits the applicable command.  If no command is found, it will prompt the user to input a command.
+
+Lines 44-68 is the Bands In Town search criteria. Axios creates a call and returns a data array that is then constructed with console.log's to access the relevant information.  This return is shown to the user and also saved in a log.text file.
+
+Lines 70-101 is the OMDB search criteria. Axios creates a call and returns a data array that is then constructed with console.log's to access the relevant information.  This return is shown to the user and also saved in a log.text file.
+
+Lines 102-138 is the Spotify search criteria. Spotify creates a call and returns a data array that is then constructed with console.log's to access the relevant information.  This return is shown to the user and also saved in a log.text file.
--- liri.js
+++ liri.js
@@ -5,12 +5,13 @@ var keys = require("./keys");
 var axios = require("axios");
 var moment = require("moment");
 var Spotify = require('node-spotify-api');
-//var spotify = new Spotify(keys.spotify)
 var fs = require("fs");
 
-//Variables for the arguments to be entered by the user
+//Captures the Variables for the search items to be entered by the user
+//Captures the command for part of the switch statement will run
 var appCommand = process.argv[2];
 console.log("App Command is: " + appCommand);
+//Captures the item they are searching for
 var userSearch = process.argv.slice(3).join(" ");
 console.log("User Search is: " + userSearch);
 
@@ -26,15 +27,15 @@ function runLiri(appCommand, userSearch) {
          break;
 
       case "spotify-this-song":
-         getSpotify(userSearch)
+         getSpotify(userSearch);
          break;
 
       case "do-what-it-says":
-         getDoWhatItDo();
+         getRead();
          break;
          //If user doesn't enter anything
          default:
-         console.log("YO! Buttercup! Enter one of the following PHRASES! concert-this, spotify-this-song, or movie-this. ")
+         console.log("YO! Buttercup! Enter one of the following PHRASES! concert-this, spotify-this-song, or movie-this! ")
 
    }
 
@@ -48,15 +49,24 @@ function getBandsInTown(artist) {
 
    axios.get(bandsInTownUrl).then(function (response) {
       //console.log(response.data)
-      console.log("===============================================");
-      console.log("Name of the Band: " + userSearch)
-      console.log("Name of the Venue: " + response.data[0].venue.name);
-      console.log("Venue location: " + response.data[0].venue.city);
-      console.log("Date of the Event :" + moment(response.data[0].datetime).format('MM/DD/YYYY'));
-   })
+      console.log("****************************************************");
+      console.log("Name of the Band: " + userSearch + "\r\n")
+      console.log("Name of the Venue: " +response.data[0].venue.name+"\r\n");
+      console.log("Venue location: " + response.data[0].venue.city + "\r\n");
+      console.log("Date of the Event :" + moment(response.data[0].datetime).format('MM/DD/YYYY') + "\r\n");
 
+   //Appends the search results to a text file name "log"
+   var logConcert = ("*****Bands In Town Log Entry*****" + 
+   "\nMusician - " + userSearch + 
+   "\nVenue - " + response.data[0].venue.name + 
+   "\nDate -" + moment(response.data[0].datetime).format('MM/DD/YYYY')+ "\n" + "\r\n");
+      fs.appendFile("log.txt", logConcert, function (err){
+      if(err) throw err
+});
+
+   })
 }
-//=========================================================================================================
+//=========================FUnciton to Search OMDB======================================
 function getOMDB(movie) {
 
    var OmdbUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
@@ -64,14 +74,29 @@ function getOMDB(movie) {
    axios.get(OmdbUrl).then(function (response) {
       //console.log(response.data); 
       console.log("==================================================")
-      console.log("Movie Title: " + response.data.Title)
-      console.log("Year Released: " + response.data.Year)
-      console.log("IMDB Rating: " + response.data.imdbRating)
-      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
-      console.log("Country Where Produced: " + response.data.Country)
-      console.log("Language: " + response.data.Language)
-      console.log("Plot: " + response.data.Plot)
-      console.log("Actors: " + response.data.Actors)
+      console.log("Movie Title: " + response.data.Title + "\r\n")
+      console.log("Year Released: " + response.data.Year + "\r\n")
+      console.log("IMDB Rating: " + response.data.imdbRating + "\r\n")
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\r\n")
+      console.log("Country Where Produced: " + response.data.Country + "\r\n")
+      console.log("Language: " + response.data.Language + "\r\n")
+      console.log("Plot: " + response.data.Plot + "\r\n")
+      console.log("Actors: " + response.data.Actors + "\r\n")
+      //Appends the search results to a text file name "log"
+      var logMovie = ("***** OMDB Entry *****"  + 
+      "\nMovie - " + response.data.Title +
+      "\nReleased: " + response.data.Year+ 
+      "\nRating: " + response.data.imdbRating +
+      "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+      "\nCountry Where Produced: " + response.data.Country +
+      "\nLanguage: " + response.data.Language+ 
+      "\nPlot: " + response.data.Plot +
+      "\nActors: " + response.data.Actors +"\n" +"\r\n")
+
+         fs.appendFile("log.txt", logMovie, function (err){
+         if(err) throw err
+});
+
    })
 }
 //=============Get Spotify Songs and Information================================================================
@@ -80,23 +105,60 @@ function getSpotify(songName) {
    var spotify = new Spotify(keys.spotify)
   // console.log("Spotify Key: " + spotify);
 
+  //HA-HA, I'm sorry. I just couldn't bring myself to put "The Sign"
+  if(!songName){
+     songName ="Overkill";
+  }
+
    spotify.search({type:'track', query: songName}, function (err, data) {
        if (err) {
            return console.log('Error occurred: ' + err);
        }
 
-       console.log("==========================================")
+       console.log("***************************************************************")
        //console.log("Data.Tracks.items is " + JSON.stringify(data.tracks.items[0]))
 
-       console.log("Song Searched For: " + userSearch);
-       console.log("Artist(s): ", data.tracks.items[0].album.artists[0].name)
-       console.log("Album Name: ", data.tracks.items[0].album.name)
-       console.log("Relase Date: ", data.tracks.items[0].album.release_date)
-       console.log("Preview Link: ", data.tracks.items[0].preview_url)
-   });
+       console.log("Song Searched For: " + userSearch+"\r\n");
+       console.log("Artist(s): ", data.tracks.items[0].album.artists[0].name + "\r\n")
+       console.log("Album Name: ", data.tracks.items[0].album.name + "\r\n")
+       console.log("Relase Date: ", data.tracks.items[0].album.release_date +"\r\n")
+       console.log("Preview Link: ", data.tracks.items[0].preview_url+ "\r\n")
+
+       //Appends the search results to a text file name "log"
+       var logSong = ("***** Spotify Log Entry *****" + 
+       "\nSong: " + userSearch +
+       "\nArtist: - " + data.tracks.items[0].album.artists[0].name +
+       "\nAlbum Name: "+ data.tracks.items[0].album.name +
+       "\mRelase Date: "+ data.tracks.items[0].album.release_date+
+       "\nPreview Link: " +data.tracks.items[0].preview_url+"\n" +"\r\n");
+       fs.appendFile("log.txt", logSong, function (err){
+          if(err) throw err
+       });
+
+      });
+
+
+//=============Random Spotify Songs and Information=================================//
+function getRead() {
+   fs.readFile("random.txt", "utf8", function(error, data) {
+
+      // If the code experiences any errors it will log the error to the console.
+      if (error) {
+        return console.log(error);
+      }
+    
+      // We will then print the contents of data
+      console.log(data);
+    
+      // Then split it by commas (to make it more readable)
+      var dataArr = data.split(",");
+    
+      // We will then re-display the content as an array for later use.
+      console.log(dataArr);
+    
+       });
+   };
 }
-
-
 
 
 runLiri(appCommand, userSearch);
